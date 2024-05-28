edc718dcf74fd4603bc4934baa96ad050be92b24
B K Harshit <bkharshit27@gmail.com>
2024-05-29 02:06:47 +0530

    added header and footer implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { AllRoutes } from "./routes/AllRoutes";
+import { Header,Footer } from "./components";
 
 function App() {
   return (
     <div>
+      <Header />
       <AllRoutes />
+      <Footer />
     </div>
   );
 }
