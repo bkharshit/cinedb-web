9de6c228468e2e8e4065665e2764040652619255
B K Harshit <bkharshit27@gmail.com>
2024-05-30 02:08:21 +0530

    removed unused variables

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,16 @@ export const Footer = () => {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="https://linkedin.com/in/bkharshit/" target="_blank" className="hover:underline me-4 md:me-6">Linkedin</a>
+            <a href="https://linkedin.com/in/bkharshit/" target="_blank"  rel="noreferrer" className="hover:underline me-4 md:me-6">Linkedin</a>
           </li>
           <li>
-            <a href="https://github.com/bkharshit" target="_blank" className="hover:underline me-4 md:me-6">Github</a>
+            <a href="https://github.com/bkharshit" target="_blank"  rel="noreferrer" className="hover:underline me-4 md:me-6">Github</a>
           </li>
           <li>
-            <a href="https://leetcode.com/u/bkharshit/" target="_blank" className="hover:underline me-4 md:me-6">Leetcode</a>
+            <a href="https://leetcode.com/u/bkharshit/" target="_blank"  rel="noreferrer" className="hover:underline me-4 md:me-6">Leetcode</a>
           </li>
           <li>
-            <a href="https://www.instagram.com/harshitbk" target="_blank" className="hover:underline">Instagram</a>
+            <a href="https://www.instagram.com/harshitbk" target="_blank"  rel="noreferrer" className="hover:underline">Instagram</a>
           </li>
         </ul>
       </div>
